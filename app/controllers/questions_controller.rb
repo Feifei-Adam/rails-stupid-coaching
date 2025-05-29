d97383dfff6d8cab1a793b25157b6e72dfc4807d
Feifei Mabusha <f.mabusha@gmail.com>
2025-05-29 20:30:41 +0200

    coach

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,7 +7,19 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    @answer = params[:answer]
-    
+    question = params[:answer]
+
+    @answer = coach_answer(question)
+    @question = question
+  end
+ private
+  def coach_answer(question)
+    if question.end_with?("?")
+      "Silly question, get dressed and go to work!"
+    elsif question == "Im working"
+        return "great, see ya"
+    else
+      "I don't care, get dressed and go to work!"
+    end
   end
 end
